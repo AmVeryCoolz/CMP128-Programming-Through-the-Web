c3f1a325668d36daccbe88f296ba73a962fdc31f
AmVeryCoolz <jcelis.mon@gmail.com>
2025-03-04 23:23:23 -0500

    Update script.js
    bruh

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
-let quotes = { '"You are Amazing!!"','"GOOD JOB!!!"','"Never give up"','"Reach for the stars and you will get far"'};
-
+let quotes = {'"You are Amazing!!"','"Good job"','"Never give up"','"Reach for the stars and you will get far"'};
 btn.addEventListener('click',function(){
   var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
   output.innerHTML = randomQuote;
