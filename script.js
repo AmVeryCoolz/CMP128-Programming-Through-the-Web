1925b7d3aef51ae6b89d068d3f2c95aedc691fbe
AmVeryCoolz <jcelis.mon@gmail.com>
2025-03-05 20:21:56 -0500

    Update script.js
    plzz work

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let quotes = [
 function displayQuotes(){
     let randomQuotesIndex = Math.floor(Math.random()*quotes.length);
     let output=quotes[randomQuotesIndex];
-    let quotesDivElements= document.getElementById("output");
-    quotesDivElement.innerHTML=output;   
+    let quotesDiveElements= document.getElementById("output");
+    quotesDiveElement.innerHTML=output;   
 }
 //plz work im tired
